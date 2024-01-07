e4fbc570bb705eab169e4255a6f55e721312eeee
Șoaita M. Monica <soaitamonica@yahoo.com>
2024-01-08 01:23:31 +0200

    last updates

--- src/app/awd-class/awd-class.component.ts
+++ src/app/awd-class/awd-class.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatIconModule} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'awd',
@@ -9,11 +10,13 @@ import {MatIconModule} from "@angular/material/icon";
   imports: [
     MatCardModule,
     MatDividerModule,
-    MatIconModule
+    MatIconModule,
+    RouterLink
   ],
   templateUrl: './awd-class.component.html',
   styleUrl: './awd-class.component.scss'
 })
 export class AwdClassComponent {
-
+  timetableLink = "https://www.cs.ubbcluj.ro/files/orar/2023-1/disc/MME8120.html";
+  syllabus="https://www.cs.ubbcluj.ro/files/curricula/2023/syllabus/BD_sem3_MME8120_en_sanda_2023_8353.pdf";
 }
--- src/app/wp-class/wp-class.component.ts
+++ src/app/wp-class/wp-class.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatIconModule} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'wp',
@@ -9,11 +10,13 @@ import {MatIconModule} from "@angular/material/icon";
   imports: [
     MatCardModule,
     MatDividerModule,
-    MatIconModule
+    MatIconModule,
+    RouterLink
   ],
   templateUrl: './wp-class.component.html',
   styleUrl: './wp-class.component.scss'
 })
 export class WpClassComponent {
-
+  timetableLink = "https://www.cs.ubbcluj.ro/files/orar/2022-2/disc/MLG5015.html";
+  syllabus="https://www.cs.ubbcluj.ro/files/curricula/2022/syllabus/IG_sem4_MLG5015_de_sanda_2022_6761.pdf";
 }
